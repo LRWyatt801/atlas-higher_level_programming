90df1163432b0b107fe1605545b20f4f3a787bb9
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-04 15:54:46 -0500

    Add 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    n = len(argv)
+    if n == 1:
+        print("0 arguments.")
+    else:
+        if n == 2:
+            print("1 argument:")
+        else:
+            print("{} armguments:".format(n - 1))  # account for filename
+        for i in range(1, n):
+            print("{}: {}".format(i, str(argv[i])))
