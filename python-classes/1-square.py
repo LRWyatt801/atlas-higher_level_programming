95146b7027a0d7fd38c289f14544b973ef0fd51d
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-10 15:17:31 -0500

    Add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a square
+"""
+
+
+class Square:
+    """class for a square
+
+        Attributes:
+            size: size of square
+    """
+    def __init__(self, size=0):
+        self.__size = size
