9ad172676cf115bea965f637bba238686aebf5c5
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-11 13:18:19 -0500

    Add 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""defines square"""
+
+
+class Square:
+    """class for square"""
+    def __init__(self, size=0):
+        """
+        New square
+        Args:
+            size (int): size of square
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        Method that calculates area of square
+
+        Returns:
+            area of square
+        """
+        return self.__size ** 2
