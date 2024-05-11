a48f018dd78e2fa34d00efe35f04284e24deb625
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-11 13:06:29 -0500

    Add 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""defines square"""
+
+
+class Square:
+    """class for square"""
+    def __init__(self, size=0):
+        """New square
+        Args:
+            size (int): size of square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            print("size must be >= 0")
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
