2a49d2342b4190fbcc71068a8431fe9d9f4e083c
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-11 13:22:31 -0500

    Updated version with fixed raise typeerror and area method

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,9 +5,11 @@
 class Square:
     """class for square"""
     def __init__(self, size=0):
-        """Init square
+        """
+        Init square
         Attributes:
-            size: size of square"""
+            size: size of square
+        """
         self.size = size
 
     @property
@@ -17,14 +19,23 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """This is the setter for size
+        """
+        This is the setter for size
         Args:
-            value: value to set size to"""
+            value: value to set size to
+        """
         if not isinstance(value, int):
-            print("size must be an integer")
-            return ValueError
+            raise TypeError("size must be an integer")
         elif value < 0:
-            print("size must be >= 0")
-            return ValueError
+            raise TypeError("size must be >= 0")
         else:
             self.__size = value
+
+    def area(self):
+        """
+        Method that calculates area of square
+
+        Returns:
+            area of square
+        """
+        return self.__size ** 2
