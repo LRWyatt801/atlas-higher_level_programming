5fdc19179918c7cc9664774fc5175e10adb3dab3
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-12 15:22:26 -0500

    Fix pycodestyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """Module for print_square function"""
 
-def print_square(size):
-	"""prints a square with the character #
 
-	Args:
-		size (int): size of square with equal sides
-	"""
-	if not isinstance(size, int):
-		raise TypeError("size must be an integer")
-	elif size < 0:
-		raise TypeError("size must be >= 0")
-	elif size == 0:
-		print("")  # print newline for size=0
-	else:
-		for i in range(size):
-			for i in range(size):
-				print("#", end="")  # print "#"
-			print("")  # print new line
+def print_square(size):
+    """prints a square with the character #
+
+    Args:
+        size (int): size of square with equal sides
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise TypeError("size must be >= 0")
+    else:
+        for i in range(size):
+            for i in range(size):
+                print("#", end="")  # print "#"
+            print("")  # print new line
