8feebea0cafa15bc0a5210dbd5329c0eab01b787
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-12 14:55:32 -0500

    Add documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,16 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """function that prints "My name is <first> <last>"
+
+    Args:
+        first_name (str): first name
+        last_name (str, optional): last name. Defaults to nothing
+
+    Raises:
+        TypeError: first name is not type str
+        TypeError: last name is not type str
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
