a6ca2da45fc74eef3bc7146502403571e2f00a9e
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-12 16:08:45 -0500

    Files for 5-text_indentation in progress

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module containing text)indentation function"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):  # check text type is str
+        raise TypeError("text must be a string")
+    
+    for c in text:
+        if
+
+    print("")
