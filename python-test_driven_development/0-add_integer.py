6ec3f8159933732b12538b70fb25e0400fb3550a
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-12 11:10:07 -0500

    Add files for 0-add_integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module for adding 2 ints together"""
+
+
+def add_integer(a, b=98):
+    """function that add 2 integers
+
+    Args:
+        a (int, float): integer to be added
+        b (int, float): integer to be added. Defaults to 98.
+
+    Raises:
+        TypeError: must be int
+        TypeError: must be int
+
+    Returns:
+        int: sum of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
