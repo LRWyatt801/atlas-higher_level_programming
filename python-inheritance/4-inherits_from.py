0585cf3e9a16bcaecd9124ad775fff5f1f5ee5f9
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-20 20:23:52 -0500

    Add 4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains inherits_from"""
+
+
+def inherits_from(obj, a_class) -> bool:
+    """Tests of obj inherits from a_class
+
+    Args:
+        obj (any): instance of a class
+        a_class (any): class to compare
+
+    Returns:
+        bool: true or false
+    """
+    return issubclass(obj, a_class)
