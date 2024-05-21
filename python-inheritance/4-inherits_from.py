e74f4df954e49ba6af92124ee365509c689ee63a
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-20 20:27:29 -0500

    fix for not same class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class) -> bool:
     Returns:
         bool: true or false
     """
-    return issubclass(obj, a_class)
+    return (issubclass(obj, a_class) and type(obj) is not a_class)
