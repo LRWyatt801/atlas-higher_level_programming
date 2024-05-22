00e9bf2a3d661a879c8f05b50dc1cdb642e1e01c
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-22 12:04:37 -0500

    Pycodestyle fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -11,25 +11,26 @@ def pascal_triangle(n: int) -> list:
     Returns:
         list: List of lists of integers represneting pascals triangle
     """
-    
+
     # matrix to store generated values
     triangle = [[0 for x in range(y + 1)]
                 for y in range(n)]
-    
+
     # iterate through every line
     for line in range(0, n):
-        
+
         # each line has number of int
         # equal to line number
         for i in range(0, line + 1):
-            
+
             # firs and last values
             # in every row are 1
-            if(i is 0 or i is line):
+            if (i is 0 or i is line):
                 triangle[line][i] = 1
-            
+
             # calc new value
             else:
-                triangle[line][i] = (triangle[line - 1][i - 1] + triangle[line - 1][i])
-    
+                triangle[line][i] = (triangle[line - 1][i - 1]
+                                     + triangle[line - 1][i])
+
     return triangle
