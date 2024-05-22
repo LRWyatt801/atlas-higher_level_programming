04f6df5aeec7629d9fbf0e623a37128a0e453ac5
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-21 19:57:45 -0500

    Add 4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Contains from_json_string function"""
+
+
+import json
+
+
+def from_json_string(my_str) -> any:
+    """Return an Python data structure represented by a JSON string
+
+    Args:
+        my_str (str): JSON string
+
+    Returns:
+        any: object represented by JSON str
+    """
+    return json.load(my_str)
