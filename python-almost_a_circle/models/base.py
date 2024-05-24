45bef70408606488b22332230f3ff106b99f94a4
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-24 09:31:33 -0500

    Pycodestyle fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,14 +5,14 @@
 class Base:
     """Base class for project"""
     __nb_objects = 0
-    
-    def __init__(self, id:int=None) -> None:
+
+    def __init__(self, id: int = None) -> None:
         """Class constructor
 
         Args:
             id (int, optional): id of object. Defaults to None.
         """
-        if id != None:
+        if id is not None:
             self.id = id
             self.__nb_objects += 1
         else:
