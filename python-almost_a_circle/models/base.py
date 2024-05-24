979fb1742adbc8cca3dee415fb200ed891b0efa4
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-24 09:27:02 -0500

    Create models folder with appropriate files

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains a base class"""
+
+
+class Base:
+    """Base class for project"""
+    __nb_objects = 0
+    
+    def __init__(self, id:int=None) -> None:
+        """Class constructor
+
+        Args:
+            id (int, optional): id of object. Defaults to None.
+        """
+        if id != None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
