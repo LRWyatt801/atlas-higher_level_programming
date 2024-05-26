ffd90f29c6d87eb50bee6a1ad19a17076fcc7028
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-25 19:26:35 -0500

    Documentation update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,7 +22,17 @@ class Base:
             self.id = self.__nb_objects
 
     def to_json_string(list_dictionaries) -> str:
+        """Returns JSON string representation
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+
+        Returns:
+            str: JSON representation
+        """
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs) -> None:
