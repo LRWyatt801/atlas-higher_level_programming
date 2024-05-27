7359460f5c008def3718b3da3d238f3cf7ed32a0
LRWyatt801 <loganwyatt1995@gmail.com>
2024-05-27 15:30:59 -0500

    Fixed save_to_file to handle list None

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,3 +50,6 @@ class Base:
             json_list = [obj.to_dictionary() for obj in list_objs]
             with open(filename, 'w') as file:
                 file.write(cls.to_json_string(json_list))
+        else:
+            with open(filename, 'w') as file:
+                file.write(cls.to_json_string(json_list))
