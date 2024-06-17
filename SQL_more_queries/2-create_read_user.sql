0569725fb101a1ce78b164d79adb7c355807a08e
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-17 16:00:08 -0500

    Add create read user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Creates a database and user
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create User
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant select permission
+GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
