0257e710c47ff9be7f19e383e6b5e4bf551d1e35
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-17 14:10:47 -0500

    Add 0-privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Lists privileges
+
+-- Lists all privileges for "user_0d_1"
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Lists all privileges for "user_0d_2"
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
