cdb621d47c0ce50bd0c92749716fe76eb1ca4d66
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-17 19:49:53 -0500

    States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- Creates a database "hbtn_0d_usa"
+-- Creates table in database called "states"
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS states (
+	id INT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+);
