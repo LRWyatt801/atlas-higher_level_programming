a48aa989ba6c0df177f8cca169bfd97ccb982b0a
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-17 20:43:26 -0500

    Add join to filter data correctly

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Lists all cities contained in database
 
-SELECT cities.id, cities.name, states.name WHERE states.id = cities.state_id
-FROM cities, states
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states on cities.state_id = states.id
 ORDER BY cities.id ASC;
