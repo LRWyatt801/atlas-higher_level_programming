da206665247c63266f3a3acfbdb78d208c937700
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-17 20:38:46 -0500

    Try select syntax

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
 -- Lists all cities contained in database
 
-SELECT id, name, (
-	SELECT name FROM states WHERE id = cities.state_id
-)
-FROM cities
+SELECT cities.id, cities.name, states.name WHERE states.id = cities.state_id
+FROM cities, states
 ORDER BY cities.id ASC;
