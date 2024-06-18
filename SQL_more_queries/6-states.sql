a24500683adcc4ea5cafd90224c613671462fe8c
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-17 20:00:32 -0500

    Fix syntax error.  To use new database

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,9 +3,10 @@
 
 -- Create database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
+-- Use database
+USE hbtn_0d_usa;
 -- Create table
 CREATE TABLE IF NOT EXISTS states (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
