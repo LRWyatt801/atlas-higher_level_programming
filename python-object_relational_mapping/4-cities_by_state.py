ea405ec9dba841ce44e721db675a16e80c5d96de
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-19 12:05:36 -0500

    pycode fix

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -28,8 +28,8 @@ def list_cities(username, passwd, database):
 
     # Execute query
     cur.execute("SELECT cities.id, cities.name, states.name FROM states "
-        + "INNER JOIN cities ON cities.state_id = states.id "
-        + "ORDER BY cities.id")
+                + "INNER JOIN cities ON cities.state_id = states.id "
+                + "ORDER BY cities.id")
 
     rows = cur.fetchall()
     for row in rows:
