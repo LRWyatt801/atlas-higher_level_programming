114fe2df5e05795c4ffd881adef9cd6364878ba2
Logan Wyatt <loganwyatt1995@gmail.com>
2024-07-29 15:36:12 -0500

    Store website in file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length <= 2) {
+  console.error('Usage: <api url> <movie id>');
+}
+
+const url = process.argv[2];
+const fileName = process.argv[3];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+  }
+  fs.writeFile(`${fileName}`, body, 'utf-8', function(err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
